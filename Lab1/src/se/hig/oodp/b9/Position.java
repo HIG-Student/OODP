15a9107b7451d8d488c95587e1a06aa9cf7cf943
Viktor Hanstorp <ndi14vhp@student.hig.se>
2015-09-11 10:52:27 +0200

    Fixed

--- Lab1/src/se/hig/oodp/b9/Position.java
+++ Lab1/src/se/hig/oodp/b9/Position.java
@@ -2,25 +2,38 @@ package se.hig.oodp.b9;
 
 import se.hig.oodp.Vertex2D;
 
-public class Position extends Vertex2D{
-
-
-	public Position(double x, double y){
-		
+public class Position extends Vertex2D
+{
+	
+	public Position(double x, double y)
+	{
 		super (x,y);
-		
 	}
-	public Position add(Position pos) {
+	
+	public Position add(Position pos) 
+	{
 		return new Position(this.getX()+pos.getX(),this.getY()+pos.getY());
 	}
-	public Position sub(Position pos) {
+	
+	public Position sub(Position pos) 
+	{
 		return new Position(this.getX()-pos.getX(),this.getY()-pos.getY());
 	}
-	public Position div(double scale) {
+	
+	public Position div(double scale) 
+	{
 		return new Position(this.getX()/scale,this.getY()/scale);
 	}
-	public Position mul(double scale) {
+	
+	public Position mul(double scale) 
+	{
 		return new Position(this.getX()*scale,this.getY()*scale);
 	}
+	
+	public boolean isCloseTo(Position position,double epsilon)
+	{
+		return	Math.abs(this.getX() - position.getX()) <= epsilon &&
+				Math.abs(this.getY() - position.getY()) <= epsilon;
 	}
+}
 	
