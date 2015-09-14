5252b86e0ccc238b11f3c5cb84026e0ac50dd5bc
Viktor Hanstorp <ndi14vhp@student.hig.se>
2015-09-14 19:28:13 +0200

    Fixed bonus

--- Lab1/src/se/hig/oodp/b9/u1_bonus/Polygon.java
+++ Lab1/src/se/hig/oodp/b9/u1_bonus/Polygon.java
@@ -3,91 +3,16 @@ package se.hig.oodp.b9.u1_bonus;
 import java.util.Vector;
 
 import se.hig.oodp.Vertex2D;
+import se.hig.oodp.b9.u1_3.PolyShape;
 
 
 // TODO Tests!
 
-public class Polygon 
-{
-	Vertex2D[] positions;
-	
-	Polygon(Vertex2D[] positions)
+public class Polygon extends PolyShape
+{	
+	Polygon(Vertex2D[] positions) 
 	{
-		this.positions = positions.clone();
-	}
-	
-	public Vertex2D[] getPositions()
-	{
-		return positions.clone();
-	}
-	
-	public void moveTo(Vertex2D position)
-	{
-		Vertex2D center = getCenter();
-		
-		for(int i = 0; i < positions.length;i++)
-			positions[i] = positions[i].moveBy(-center.getX(), -center.getY()).moveBy(position.getX(), position.getY());
-	}
-	
-	public void moveBy(double x, double y)
-	{
-		for(int i = 0; i < positions.length;i++)
-			positions[i] = positions[i].moveBy(x, y);
-	}
-	
-	public void scale(double scale)
-	{
-		Vertex2D center = getCenter();
-		
-		for(int i = 0; i < positions.length;i++)
-			positions[i] = positions[i].scale(center,scale,scale);
-	}
-	
-	public void rotate(double angle)
-	{
-		Vertex2D center = getCenter();
-		
-		for(int i = 0; i < positions.length;i++)
-			positions[i] = positions[i].rotate(center,angle);
-	}
-	
-	// TODO Fix better centroid
-	
-	public Vertex2D getCenter()
-	{
-		double sum_x = 0;
-		double sum_y = 0;
-		
-		for(Vertex2D p : positions)
-		{
-			sum_x += p.getX();
-			sum_y += p.getY();
-		}
-		
-		return new Vertex2D(sum_x / positions.length , sum_y / positions.length);
-	}
-	
-	public void remove()
-	{
-		
-	}
-	
-	public void draw()
-	{
-		
-	}
-	
-	@Override
-	public String toString()
-	{
-		String positions_str = "";
-		
-		for(Vertex2D p : positions)
-		{
-			positions_str += p.toString() + " to ";
-		}
-		
-		return "Polygon " + positions_str.substring(0, positions_str.length() - 4);
+		super(positions);
 	}
 	
 	public class Builder
@@ -97,6 +22,11 @@ public class Polygon
 		public void clear()
 		{
 			positions.removeAllElements();
+		}
+		
+		public void add(Vertex2D position)
+		{
+			positions.add(position);
 		}
 		
 		public Polygon create() throws Exception
