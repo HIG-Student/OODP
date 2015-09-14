c0e2ee3aefea64e40f08b56c4e0f88bad3aaabff
Viktor Hanstorp <ndi14vhp@student.hig.se>
2015-09-14 19:29:59 +0200

    Fixed

--- Lab1/src/se/hig/oodp/b9/u1_bonus/Square.java
+++ Lab1/src/se/hig/oodp/b9/u1_bonus/Square.java
@@ -1,6 +1,17 @@
 package se.hig.oodp.b9.u1_bonus;
 
-public class Square 
-{
+import se.hig.oodp.Vertex2D;
+import se.hig.oodp.b9.u1_3.Rectangle;
 
+public class Square extends Rectangle
+{
+	public Square(Vertex2D position, double size) 
+	{
+		super(position, size, size);
+	}
+	
+	public Square(Vertex2D position, double size, double rotation) 
+	{
+		super(position, size, size,rotation);
+	}
 }
