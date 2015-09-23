4305d11ff635660e8d48d25cfbda53086f39f184
Viktor Hanstorp <ndi14vhp@student.hig.se>
2015-09-23 08:32:15 +0200

    modified (incorrect square)

--- Lab1/src/se/hig/oodp/FigureHandler.java
+++ Lab1/src/se/hig/oodp/FigureHandler.java
@@ -7,7 +7,7 @@ public interface FigureHandler
 	public void createLine (double x0, double y0, double x1, double y1);
 	public void createPoint (double x, double y);
 	public void createRectangle (double x, double y, double a, double b);
-	public void createSquare (double x, double y, double a, double b);
+	public void createSquare (double x, double y, double a); // edited by: ndi14vhp
 	public void createTriangle (double vx0, double vy0, double vx1, double vy1, double vx2, double vy2);
 	public void removeAll ();
 }
